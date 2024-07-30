17521a99ac7287484f2d76522c7809e802f4acbd
NelsonRookie <real.nelson92@gmail.com>
2024-07-31 00:01:46 +0800

    instantiate new object, and put event listener to the button, and create aimation

--- script.js
+++ script.js
@@ -28,9 +28,49 @@ class Player {
         this.height = proportionalSize(40);
 
     }
-
+    // use to create the characters
     draw() {
         ctx.fillStyle = "#99c9ff" ;
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
+    // ensure that character are continually drawn on the screen
+    update() {
+        this.draw();
+        this.position += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        if(this.position.y + this.height + this.velocity.y <= canvas.height) {
+            if(this.position.y < 0) {
+                this.position.y = 0;
+                this.velocity.y = gravity;
+            }
+            this.velocity.y = gravity;
+        }else {
+            this.velocity.y = 0;
+        }
+
+        if(this.position.y < this.width) {
+            this.position.x = this.width
+        }  
+
+        if(this.position.x >= canvas.width - this.width * 2) {
+            this.position.x = canvas.width - this.width * 2;
+        }
+    }
+}
+
+const player = new Player();
+const startGame = () => {
+    canvas.style.display = 'block';
+    canvas.style.display = 'none';
+    player.draw();
+}
+
+startBtn.addEventListener('click', startBtn);
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    //clear the canvas before the next animation
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
 }
